e78163437032f554be06517c863413b532e5ab8f
Dexter Shepherd <dexter@kadenze.com>
2016-09-17 16:02:25 -0700

    added linux app exports

--- exports/application.linux32/source/five_hundred_frames_of_noise.java
+++ exports/application.linux32/source/five_hundred_frames_of_noise.java
@@ -1,16 +1,32 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class five_hundred_frames_of_noise extends PApplet {
+
 Walker[] walkers;
 
 float strength, scale, cluster_width, cluster_height;
 
 
-void setup(){
+public void setup(){
   colorMode(HSB, 100, 100, 100, 100);
-  size(700, 700);
+  
   strength = random(40);
   scale = random(30);
   cluster_width = random(100);
   cluster_height = random(100);
-  walkers = new Walker[int(random(1, 50)) * 2];
+  walkers = new Walker[PApplet.parseInt(random(1, 50)) * 2];
   for(int i = 0; i < walkers.length - 1; i+=2){
     walkers[i] = new Walker((width/2) - (cluster_width/2),
                             (height/2) - (cluster_height/2) + map(i, 0, walkers.length, 0, cluster_height),
@@ -28,7 +44,7 @@ void setup(){
   fill((base_hue + random(25, 75)) % 100, random(10, 70), 100, random(15));
 }
 
-void draw(){
+public void draw(){
   beginShape();
   for(int i = 0; i < walkers.length - 1; i+=2){
     walkers[i].move();
@@ -59,17 +75,27 @@ class Walker{
     scale = _scale;
   }
 
-  void move(){
+  public void move(){
     float angle = noise(map(loc.x, 0, width, 0, scale), 
                         map(loc.y, 0, height, 0, scale)) * strength;
 
     acc = new PVector(cos(angle), sin(angle));
     vel.add(acc);
     loc.add(vel);
-    vel.mult(0.1);
+    vel.mult(0.1f);
   }
 }
 
-void mouseClicked(){
+public void mouseClicked(){
   setup();
+}
+  public void settings() {  size(700, 700); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "five_hundred_frames_of_noise" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
