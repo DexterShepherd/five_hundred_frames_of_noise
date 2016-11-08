f7a60cce9f788df707b871f03f6a542a05a96028
Dexter Shepherd <dexter@kadenze.com>
2016-11-07 19:11:33 -0800

    added build scripts

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Twit = require('twit');
+const fs = require('fs');
+const keys = require('./keys');
+const renderer = require('./renderer');
+
+const client = new Twit(keys);
+
+
+renderer.render(function() {
+  fs.readFile('./render.png', { encoding: 'base64' }, function(err, data) {
+    if(err){
+      return console.log(err);
+    }
+    client.post('media/upload', { media_data: data }, function(err, data, response) {
+      var id = data.media_id_string
+      var alt = "500 frames of noise";
+      var meta = { media_id: id, alt_text: { text: alt } };
+
+      client.post('media/metadata/create', meta, function(err, data, response) {
+        if(err){
+          console.log(err);
+        } else {
+          var params = { status: '500', media_ids: [id] } 
+
+          client.post('statuses/update', params, function(err, data, response) {
+            console.log('Tweeted!');
+          });
+        }
+      });
+    });
+  });
+});
---
+++ renderer.js
@@ -0,0 +1,29 @@
+const phantom = require('phantom');
+
+console.log('building 500 frames of noise' + new Date().toLocaleString());
+
+
+exports.render = function(callback) {
+  var phantom_instance;
+  var phantom_page;
+
+  phantom.create().then(instance => {
+    phantom_instance = instance; 
+    return instance.createPage();
+  }).then(page => {
+    phantom_page = page;
+    return page.open("http://dexterjshepherd.com/five_hundred_frames_of_noise");
+  }).then(status => {
+    console.log(status);
+    if(status === "success") {
+      setTimeout(function() {
+        phantom_page.render('render.png');
+        phantom_instance.exit();
+        setTimeout(function(){ callback() }, 1000);
+      }, 10000);
+    } else {
+      console.log('exiting with errors');
+      phantom_instance.exit();
+    }
+  });
+}
